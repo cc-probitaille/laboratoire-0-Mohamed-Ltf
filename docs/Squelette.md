a12c668e0b33e250933b1e7bfce9e2be79b3dbcb
Mohamed Loutfi <mohamed.loutfi@dawsoncollege.com>
2025-09-10 22:02:23 -0400

    Completé Étape 3

--- docs/Squelette.md
+++ docs/Squelette.md
@@ -194,6 +194,7 @@ Voici la même figure, mais sous forme de diagramme de séquence avec l'acteur. 
 
 ![Diagramme de séquence, demarrerJeu](http://www.plantuml.com/plantuml/proxy?cache=no&fmt=svg&src=https://raw.githubusercontent.com/profcfuhrmanets/log210-jeu-de-des-node-express-ts/master/docs/modeles/rdcu-demarrerJeu.puml)
 
+
 #### Opération: `jouer(nom:String)`
 
 **Postconditions**
@@ -215,4 +216,10 @@ Voici la même figure, mais sous forme de diagramme de séquence avec l'acteur. 
 
 ![Diagramme de séquence, terminerJeu](http://www.plantuml.com/plantuml/proxy?cache=no&fmt=svg&src=https://raw.githubusercontent.com/profcfuhrmanets/log210-jeu-de-des-node-express-ts/master/docs/modeles/rdcu-terminerJeu.puml)
 
+
+
+
 [README.md](../README.md)
+
+
+ 
--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,27 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
+import supertest from 'supertest'
 import 'jest-extended';
+import app from '../../src/app';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+const request = supertest(app);
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  beforeAll( async () => {
+    // Creer 2 joueurs
+    const joueur1 = await request.get('/api/v1/jeu/demarrerJeu?nom=Jean-Marc');
+    const joueur2 = await request.get('/api/v1/jeu/demarrerJeu?nom=Pierre');
+  });
+
+  it('devrait redémarrer le jeu avec succes', async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+  });
+
+  //TO CHECK
+  it('devrait avoir aucun joueur', async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.body.joueurs.size).toBe(0);
   });
 });
